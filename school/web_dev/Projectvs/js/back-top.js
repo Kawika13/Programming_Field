58d783b4087fbdec09fc9e32703c8d884ad41b6e
Kawika13 <47378534+Kawika13@users.noreply.github.com>
2021-05-17 00:21:35 +0200

    Add files via upload

--- school/web_dev/Projectvs/js/back-top.js
+++ school/web_dev/Projectvs/js/back-top.js
@@ -5,6 +5,7 @@ var btt = document.getElementById("backtop"),   // id backtop
     offset = 200,                               // base position
     scrollPos, docHeight;                       // define scrollPos e docHeight (actual position and the max document height)
 
+    
 /* Scorlling function (used for the smooth effect) */
 function scrolling(){
     window.scrollTo({
@@ -17,18 +18,18 @@ function scrolling(){
 /* set the height of the document */
 docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.offsetHeight, docElem.scrollHeight, docElem.clientHeight);   //find the max from these elements
 
+
 /* set the appropriate offset's value */
 if (docHeight != undefined) //if docHeight isn't undefined, the offset is equal to it divided for 4
     {
     offset = docHeight / 4;
     }
-console.log(docHeight);
-console.log(offset);
+
 
 /* event listener for the scroll position */
 window.addEventListener("scroll", 
     function(event){
-        scrollPos = body.scrollTop || docElem.scrollTop;        //
+        scrollPos = body.scrollTop || docElem.scrollTop;
         btt.className = (scrollPos > offset) ? "visible" : "";  //if the scroll position is higher than the offset (base position), then the "backtop"'s class is "visible", otherwise not
     }
 );
--- school/web_dev/Projectvs/js/nav-bar.js
+++ school/web_dev/Projectvs/js/nav-bar.js
@@ -1,20 +1,56 @@
-var path = window.location.pathname;
-var page = path.split("/").pop();
-console.log( page );
-
+/* Navigation Bar*/
 function nav_bar(){
     document.write(`
 
     <div class="head">
         <img src="../Immagini/logo.png" class="logo"/>
         <ul class="nav_bar">
-            <li><a href="index.html?mn=home">Home</a></li>
-            <li><a href="istituto.html?mn=ist">Isitituto</a></li>
-            <li><a href="didattica.html?mn=did">Didattica</a></li>
-            <li><a href="modulistica.html?mn=mod">Modulistica</a></li>
-            <li><a href="news.html?mn=news">News</a></li>
+            <li id="index"><a href="index.html?mn=home">Home</a></li>
+            <li id="ist"><a href="istituto.html?mn=ist">Isitituto</a></li>
+            <li id="did"><a href="didattica.html?mn=did">Didattica</a></li>
+            <li id="mod"><a href="modulistica.html?mn=mod">Modulistica</a></li>
+            <li id="news"><a href="news.html?mn=news">News</a></li>
         </ul>
     </div>
     
     `);
 }
+
+nav_bar();
+
+
+/* Declaring Variables */
+var path = window.location.pathname,
+    page = path.split("/").pop(),
+    index = document.getElementById("index"),
+    ist = document.getElementById("ist"),
+    did = document.getElementById("did"),
+    mod = document.getElementById("mod"),
+    news = document.getElementById("news");
+
+
+/* "active" class assigment */
+if (page == "index.html")
+    {
+    index.className = "active";
+    }
+else if (page == "istituto.html")
+    {
+    ist.className = "active";
+    }
+else if (page == "didattica.html")
+    {
+    did.className = "active";
+    }
+else if (page == "modulistica.html")
+    {
+    mod.className = "active";
+    }
+else if (page == "news.html")
+    {
+    news.className = "active";
+    }
+else
+    {
+    alert("Something probably is going wrong, please reset the page");
+    }
