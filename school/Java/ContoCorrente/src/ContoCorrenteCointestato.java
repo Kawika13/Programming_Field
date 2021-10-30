f16c003f3d698639d14767492cdcef5733dcc431
Kawika13 <47378534+Kawika13@users.noreply.github.com>
2021-10-30 12:01:23 +0200

    Add files via upload

---
+++ school/Java/ContoCorrente/src/ContoCorrenteCointestato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Studente
+ */
+public class ContoCorrenteCointestato extends ContoCorrente{
+
+    public ContoCorrenteCointestato(String name, String surname, String fiscalCode, int age) {
+        super(name, surname, fiscalCode, age);
+    }
+    
+}
