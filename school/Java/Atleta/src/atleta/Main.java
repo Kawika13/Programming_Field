053777c2d8b64230785466d7882692841deee6ea
Kawika13 <47378534+Kawika13@users.noreply.github.com>
2021-11-02 12:59:51 +0100

    Update Main.java

--- school/Java/Atleta/src/atleta/Main.java
+++ school/Java/Atleta/src/atleta/Main.java
@@ -1,23 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package atleta;
 
-/**
- *
- * @author Studente
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        // int age, int weight, int height, String name, String surname, boolean visitaMedica
-        Atleta x = new Atleta(22, 75.6, 181, "Paolo", "Mazzini", false);
+        // int age, int weight, int height, String name, String surname, String sport, boolean visitaMedica
+        Atleta x = new Atleta(22, 75.6, 181, "Paolo", "Mazzini", "Hockey", false);
         
         x.setCountry("Italia");
         x.effettuaVisita();
