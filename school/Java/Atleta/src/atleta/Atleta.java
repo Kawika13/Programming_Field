83423eeb4ee41996f5cec5727972bc5c49cec680
Kawika13 <47378534+Kawika13@users.noreply.github.com>
2021-11-02 13:00:17 +0100

    Update Atleta.java

--- school/Java/Atleta/src/atleta/Atleta.java
+++ school/Java/Atleta/src/atleta/Atleta.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package atleta;
+
 
 import java.util.*;
 
-/**
- *
- * @author Studente
- */
+
 public class Atleta {
     
     private int age;
