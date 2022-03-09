fa1cfa06d24846fcdfd864e074aef05c7b764111
Kawika13 <47378534+Kawika13@users.noreply.github.com>
2022-03-09 13:34:31 +0100

    Add files via upload

---
+++ school/web_dev/Index/js/nav_bar.js
@@ -0,0 +1,20 @@
+
+/* Navigation Bar */
+function nav_bar(){
+    document.write(`
+    <header>
+        <a href="index.html">
+            <img src="../img/58114397440.png" alt="There's a logo here">
+        </a>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="form.html">Form</a></li>
+                <li><a href="fattoriale.html">Fattoriale</a></li>
+            </ul>
+        </nav>
+    </header>
+    `);
+}
+
+nav_bar();
